5e504a300d0c10769b2665ff969352a39ed6a639
usuario <usuario@192.168.32.38>
2026-02-24 14:37:33 +0100

    commit2

--- jrc/Proyecto/src/ejemplo_jrc/holaMundo.java
+++ jrc/Proyecto/src/ejemplo_jrc/holaMundo.java
@@ -4,6 +4,9 @@ public class holaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		
+		System.out.println("Mensaje 1");
+		System.out.println("Mensaje 2");
 
 	}
 

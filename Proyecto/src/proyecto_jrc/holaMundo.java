f628efeac3949be2d14fa706e381c6657d1539e7
usuario <usuario@192.168.32.38>
2026-02-24 14:08:34 +0100

    commit1

---
+++ Proyecto/src/proyecto_jrc/holaMundo.java
@@ -0,0 +1,10 @@
+package proyecto_jrc;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
---
+++ jrc/Proyecto/src/ejemplo_jrc/holaMundo.java
@@ -0,0 +1,10 @@
+package ejemplo_jrc;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
